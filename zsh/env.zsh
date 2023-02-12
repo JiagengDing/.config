b730cb4c0202a90c5355665748dc9e91cb938b71
jiagengding <jiageng.ding@gmail.com>
2023-02-12 23:12:40 +0000

    update env.zsh

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -5,6 +5,7 @@ export GPG_TTY=$TTY
 
 
 export PATH=$HOME/bin:/usr/local/bin:$HOME/.local/bin:$PATH
+export PATH="/usr/local/opt/libressl/bin:$PATH
 export XDG_CONFIG_HOME=$HOME/.config
 export LOCALBIN=$XDG_CONFIG_HOME/bin
 export PATH=$PATH:$HOME/go/bin
