86125fe44194d36535e3c4e8151c9636970e9135
jiagengding <jiageng.ding@gmail.com>
2024-07-06 11:14:13 +0800

    add yazi plugins

--- yazi/init.lua
+++ yazi/init.lua
@@ -43,3 +43,87 @@ end
 require("git-status"):setup {
 	style = "linemode" -- beside or linemode
 }
+
+-- Show yatline
+require("yatline"):setup({
+	section_separator = { open = "", close = "" },
+	part_separator = { open = "", close = "" },
+
+	style_a = {
+		fg = "black",
+		bg_mode = {
+			normal = "#6da4fb",
+			select = "#d79921",
+			un_set = "#d65d0e"
+		}
+	},
+	style_b = { bg = "#394263", fg = "#ebdbb2" },
+	style_c = { bg = "#1e2336", fg = "#a89984" },
+
+	permissions_t_fg = "green",
+	permissions_r_fg = "yellow",
+	permissions_w_fg = "red",
+	permissions_x_fg = "cyan",
+	permissions_s_fg = "darkgray",
+
+	tab_width = 20,
+
+	selected = { icon = "󰻭", fg = "yellow" },
+	copied = { icon = "", fg = "green" },
+	cut = { icon = "", fg = "red" },
+
+	total = { icon = "󰮍", fg = "yellow" },
+	succ = { icon = "", fg = "green" },
+	fail = { icon = "", fg = "red" },
+	found = { icon = "󰮕", fg = "blue" },
+	processed = { icon = "󰐍", fg = "green" },
+
+	header_line = {
+		left = {
+			section_a = {
+				{ type = "line", custom = false, name = "tabs", params = { "left" } },
+			},
+			section_b = {
+			},
+			section_c = {
+			}
+		},
+		right = {
+			section_a = {
+				-- 			{ type = "string", custom = false, name = "date", params = { "%A, %d %B %Y" } },
+			},
+			section_b = {
+				-- 			{ type = "string", custom = false, name = "date", params = { "%X" } },
+			},
+			section_c = {
+			}
+		}
+	},
+
+	status_line = {
+		left = {
+			section_a = {
+				{ type = "string", custom = false, name = "tab_mode" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "hovered_size" },
+			},
+			section_c = {
+				{ type = "string",   custom = false, name = "hovered_name" },
+				{ type = "coloreds", custom = false, name = "count" },
+			}
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "cursor_position" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "cursor_percentage" },
+			},
+			section_c = {
+				{ type = "string",   custom = false, name = "hovered_file_extension", params = { true } },
+				{ type = "coloreds", custom = false, name = "permissions" },
+			}
+		}
+	},
+})
